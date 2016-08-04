5aa306ae4d9491d47650a6d69039f5ad75f5ea0f
Milan Oberkirch <milan.oberkirch@catalyst.net.nz>
2016-08-04 16:09:02 +1200

    Add link to project page.

--- js/ghost-typer.js
+++ js/ghost-typer.js
@@ -2,8 +2,8 @@
 // see https://github.com/lokeshthegenius/ghost-typer/blob/master/LICENSE for tems of use.
 
 (function($){
-	$.fn.ghosttypeonce = function(options){
-    	var settings = $.extend({},$.fn.ghosttypeonce.defaults,options);
+    $.fn.ghosttypeonce = function(options){
+        var settings = $.extend({},$.fn.ghosttypeonce.defaults,options);
         var elem = $(this);
         typing(0, false);
         return this;
@@ -11,25 +11,25 @@
         function typing(slicer) {
             setTimeout(function () {
                 slicer += 1;
-								if (settings.message[slicer] == '<') {
-									while (settings.message[slicer] != '>') {
-										slicer += 1
-									}}
+                if (settings.message[slicer] == '<') {
+                    while (settings.message[slicer] != '>') {
+                        slicer += 1
+                    }}
                 elem.html(settings.message.slice(0, slicer));
                 if (slicer == settings.message.length) {
                     if(settings.callback) setTimeout(settings.callback, settings.timePause);
                     return;
-								} else {
-									typing(slicer);
-								}
+                } else {
+                    typing(slicer);
+                }
             }, settings.timeWrite);
         }
     };
     $.fn.ghosttypeonce.defaults = {
-            message: null,
-            elem: null,
-            timeWrite: 100,
-						timePause: 200,
-            callback: undefined
+        message: null,
+        elem: null,
+        timeWrite: 100,
+        timePause: 200,
+        callback: undefined
     };
 })(jQuery);
